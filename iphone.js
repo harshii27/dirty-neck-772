17e6f3241bfd8a5e0dbc449bd84ebfc8ed878fe7
Vaibhav <vaibhav7895@gmail.com>
2023-01-22 23:04:05 +0530

    done cart

--- iphone.js
+++ iphone.js
@@ -3,32 +3,32 @@ let data = [
         "image": "https://www.apple.com/v/iphone/home/bk/images/overview/compare/compare_iphone_14_pro__cjmfbiggqhpy_large.jpg",
         // "color-dot":"https://www.apple.com/v/iphone/home/bk/images/overview/compare/swatch_iphone_14_pro__c2bl98e0li4i_large.png",
         "title": "iPhone 14 Pro",
-        "text": "The ultimate iPhone.",
-        "price": 999,
+        "description": "The ultimate iPhone.",
+        "price": 149999,
         "buy": "Buy",
     },
     {
         "image": "https://www.apple.com/v/iphone/home/bk/images/overview/compare/compare_iphone_14_pro__cjmfbiggqhpy_large.jpg",
         // "color-dot":"https://www.apple.com/v/iphone/home/bk/images/overview/compare/swatch_iphone_14_pro__c2bl98e0li4i_large.png",
         "title": "iPhone 14",
-        "text": "A total powerhouse.",
-        "price": 799,
+        "description": "A total powerhouse.",
+        "price": 129999,
         "buy": "Buy",
     },
     {
         "image": "https://www.apple.com/v/iphone/home/bk/images/overview/compare/compare_iphone_14_pro__cjmfbiggqhpy_large.jpg",
         // "color-dot":"https://www.apple.com/v/iphone/home/bk/images/overview/compare/swatch_iphone_14_pro__c2bl98e0li4i_large.png",
         "title": "iPhone 13",
-        "text": "As amazing as ever.",
-        "price": 599,
+        "description": "As amazing as ever.",
+        "price": 119999,
         "buy": "Buy",
     },
     {
         "image": "https://www.apple.com/v/iphone/home/bk/images/overview/compare/compare_iphone_14_pro__cjmfbiggqhpy_large.jpg",
         // "color-dot":"https://www.apple.com/v/iphone/home/bk/images/overview/compare/swatch_iphone_14_pro__c2bl98e0li4i_large.png",
         "title": "iPhone SE",
-        "text": "Serious power. Serious value.",
-        "price": 499,
+        "description": "Serious power. Serious value.",
+        "price": 70000,
         "buy": "Buy",
     }
 ]
@@ -47,24 +47,31 @@ function display(data) {
         image.setAttribute("class", "imgs")
         let title = document.createElement("h2")
         title.innerText = element.title
-        let text = document.createElement("p")
-        text.innerText = element.text
+        let description = document.createElement("p")
+        description.innerText = element.description
         let price = document.createElement("h4")
         price.innerText = "$ " + element.price
 
         let btn = document.createElement("button")
         btn.innerText = element.buy
-        btn.setAttribute("class", "btn")
+        btn.onclick=()=>{
+            addToCart(element);
+            alert("product added to cart")
+            href="http://127.0.0.1:5502/cart1.html"
+        };
+        function addToCart(element){
+            let phone = JSON.parse(localStorage.getItem("items"))||[]
+            phone.push(element);
+            localStorage.setItem("items",JSON.stringify(data));
+            cart_count()
+        }
+        function cart_count(){
+            let phone=JSON.parse(localStorage.getItem("items"))||[];
+            let c= document.getElementById("item_count");
+            c.innerText=phone.length
+        }
 
-        btn.addEventListener("click", () => {
-            let data = JSON.parse(localStorage.getItem("buy")) || [];
-
-            data.push(element)
-            localStorage.setItem("buy", JSON.stringify(data));
-            console.log(data)
-        })
-
-        card.append(image, title, text, price, btn)
+        card.append(image, title, description, price, btn)
         container.append(card)
     });
 }
--- paymeent/payment.js
+++ paymeent/payment.js
@@ -1,59 +1,51 @@
- 
-   var form = document.getElementById("payment-form")
-  var cardNumber = document.getElementById("card-number");
-  var expirationMonth = document.getElementById("expiration-month");
-  var cvv = document.getElementById("cvv");
-  var billingZip = document.getElementById("billing-zip");
-  var year= document.getElementById("Year")
-  var namecard=document.getElementById("Name")
-  var btn=document.getElementsByClassName("submit-btn")
-  form.addEventListener("submit", function(event) {
-    // Prevent the form from being submitted
-    event.preventDefault();
 
-    // Check that all fields are filled out
-    if (!cardNumber.value && !expirationMonth.value && !cvv.value && !namecard) {
-      alert("Please fill out all fields.");
-      return;
-    }
+var form = document.getElementById("payment-form")
+var cardNumber = document.getElementById("card-number");
 
-    // Check that the card number is valid
-    if (!isValidCardNumber(cardNumber.value)) {
-      alert("Invalid card number.");
-      return;
-    }
+var cvv = document.getElementById("cvv");
+var billingZip = document.getElementById("billing-zip");
+var year = document.getElementById("Year")
+var namecard = document.getElementById("Name")
+var btn = document.getElementsByClassName("submit-btn")
 
-    // Check that the expiration date is valid
-    if (!isValidExpirationDate(expirationDate.value)) {
-      alert("Invalid expiration date.");
-      return;
-    }
+form.addEventListener("submit", function (event) {
+  // Prevent the form from being submitted
+  event.preventDefault();
+  // Check that all fields are filled out
+  if (!cardNumber.value || !cvv.value || !namecard) {
+    alert("Please fill out all fields.");
+    return;
+  }
 
-    // Check that the CVV is valid
-    if (!isValidCVV(cvv.value)) {
-      alert("Invalid CVV.");
-      return;
-    }
+  // Check that the card number is valid
+  if (cardNumber.value.length) {
+    isValidCardNumber(cardNumber.value.length)
+  }else{
+    alert("Invalid card number.");
+    return
+  }
 
-    // Check that the billing ZIP is valid
-    if (!isValidBillingZip(billingZip.value)) {
-      alert("Invalid billing ZIP.");
-      return;
-    }
+  // Check that the CVV is valid
+  if (!isValidCVV(cvv.value)) {
+    alert("Invalid CVV.");
+    return;
+  }
 
-    
-  });
-
-  function isValidCardNumber(cardNumber) {
-    
   
-    // Check that the card number is at least 12 digits long
-    if (cardNumber.length < 12) {
-      return false;
-    }
+
+
+});
+
+function isValidCardNumber(cardNumber) {
+     console.log(cardNumber)
+
+  // Check that the card number is at least 12 digits long
+  if (cardNumber == 16) {
+    return false;
+  } else {
     var sum = 0;
     var isEven = false;
-    for (var i = cardNumber.length - 1; i >= 0; i--) {
+    for (var i = cardNumber.value.length - 1; i >= 0; i--) {
       var digit = parseInt(cardNumber.charAt(i));
       if (isEven) {
         digit *= 2;
@@ -64,38 +56,32 @@
       sum += digit;
       isEven = !isEven;
     }
-  
-    return sum % 10 === 0;
   }
-  
 
-  function isValidExpirationDate(expirationDate) {
-    var today = new Date();
-    var enteredDate = new Date(expirationDate);
-    enteredDate.setMonth(enteredDate.getMonth()+1);
-    if (enteredDate < today) {
-        return false;
-    }
-    return true;
+
+  return sum % 10 === 0;
 }
+
+
+
 
 
 function isValidCVV(cvv) {
-    // Check that the CVV is 3 or 4 digits long
-    if (cvv.length < 3 || cvv.length > 4) {
-        return false;
-    }
+  // Check that the CVV is 3 or 4 digits long
+  if (cvv.length < 3 || cvv.length > 4) {
+    return false;
+  }
 
-    // Check that the CVV is a number
-    if (isNaN(cvv)) {
-        return false;
-    }
+  // Check that the CVV is a number
+  if (isNaN(cvv)) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 
-  
 
-  
+
+
 
